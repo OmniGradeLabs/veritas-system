9178b784579215ad567ca93b2ea9eda7fc968e27
namdang-fdp <namdangcoder@gmail.com>
2026-04-21 15:15:10 +0700

    init

---
+++ src/main/java/com/veritas/omnigrade/OmnigradeApplication.java
@@ -0,0 +1,13 @@
+package com.veritas.omnigrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnigradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnigradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veritas/omnigrade/OmnigradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veritas.omnigrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnigradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
