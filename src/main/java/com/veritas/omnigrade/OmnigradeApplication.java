56049363371ccea7a878b550c966026155144650
namdang-fdp <namdangcoder@gmail.com>
2026-04-21 16:01:56 +0700

    chore: setup precommit rules

--- src/main/java/com/veritas/omnigrade/OmnigradeApplication.java
+++ src/main/java/com/veritas/omnigrade/OmnigradeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OmnigradeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OmnigradeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(OmnigradeApplication.class, args);
+  }
 }
