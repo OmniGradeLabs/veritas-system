88a8707edbc65acf63f35cb6eb7300a46794e67e
namdang-fdp <namdangcoder@gmail.com>
2026-04-24 10:49:30 +0700

    feat(cache): add redis json helper

---
+++ src/main/java/com/veritas/omnigrade/infrastructure/cached/redis/model/session/SessionAuthzCache.java
@@ -0,0 +1,10 @@
+package com.veritas.omnigrade.infrastructure.cached.redis.model.session;
+
+import java.util.List;
+import java.util.UUID;
+
+// This define the value of StringRedisTemplate<Key, Value>
+// The roles and permissions are String not Entity for the minimal purpose, less coupling with DB
+// Immutable representation of a user's authorization context for caching. Decouples security
+public record SessionAuthzCache(
+    UUID userId, List<String> roles, List<String> groups, List<String> permissions) {}
